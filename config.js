13dbda117c09b2b887a79bab2b7fa75494947723
Pavel Zaletski <pavel.zaletski@gmail.com>
2019-02-04 09:19:26 +0300

    changed fb callback url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const facebookParams = {
     clientID: '612251399234373',
     clientSecret: '4e2341920972fc5c5c0052e5f94fe66c',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    callbackURL: 'https://epam-education.herokuapp.com/auth/facebook/callback'
 };
   
 module.exports = facebookParams;
